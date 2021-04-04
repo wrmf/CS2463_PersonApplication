92655dc223dccedcaf198d4ac6ece0088b22fa0c
wrmf <31448920+wrmf@users.noreply.github.com>
2021-04-04 13:02:28 -0500

    Info + other buttons
    Info button works now. Other buttons have been added but have no functionality

--- src/PersonApplicationGUI.java
+++ src/PersonApplicationGUI.java
@@ -1,18 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
+import java.io.*;
+import java.net.*;
 import javax.swing.*;
 
 public class PersonApplicationGUI extends JPanel implements KeyListener, ActionListener{
 	private static final int WIDTH = 1200;
 	private static final int HEIGHT = 800;
 	JPanel northPanel = new JPanel();
-	private JMenuItem quitButton, helpButton, sourceButton;
+	private JMenuItem quitButton, helpButton, sourceButton, newButton, saveButton, saveAsButton, saveChangesButton, openButton;
 	JFrame frame = new JFrame();
+	JFrame helpFrame = new JFrame();
 	private JMenu fileMenu, helpMenu;
 	private JMenuBar menuBar;
 	
@@ -29,20 +27,33 @@ public class PersonApplicationGUI extends JPanel implements KeyListener, ActionL
 		menuBar = new JMenuBar();
 		quitButton = new JMenuItem("Exit");
 		helpButton = new JMenuItem("Info");
+		newButton = new JMenuItem("New");
+		saveButton = new JMenuItem("Save");
+		saveAsButton = new JMenuItem("Save As");
+		openButton = new JMenuItem("Open");
 		sourceButton = new JMenuItem("Source");
+		
+		//Add buttons to menus
+		fileMenu.add(newButton);
+		fileMenu.add(openButton);
+		fileMenu.add(saveButton);
+		fileMenu.add(saveAsButton);
 		fileMenu.add(quitButton);
 		helpMenu.add(helpButton);
 		helpMenu.add(sourceButton);
 		
+		//add menus to menu bar
 		menuBar.add(fileMenu);
 		menuBar.add(helpMenu);
 		
+		//Quit button closes program/gui
 		quitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
 		
+		//Source button opens github
 		sourceButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -57,6 +68,13 @@ public class PersonApplicationGUI extends JPanel implements KeyListener, ActionL
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
+			}
+		});
+		
+		//Help button gives some amount of help
+		helpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "This program helps to easily view Person/RegisteredPerson/OCCCPerson","Help", JOptionPane.NO_OPTION);
 			}
 		});
 		
